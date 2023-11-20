45dc947c4dfdbb91667351ba62a0a99d91c92f03
Geert van Geest <geert.vangeest@bioinformatics.unibe.ch>
2023-11-20 14:26:47 +0100

    adds sequencing machine graphs

--- Docker/vscode/run_container_locally.sh
+++ Docker/vscode/run_container_locally.sh
@@ -17,8 +17,29 @@ rocker/rstudio
 docker run \
 --rm \
 -p 8443:8443 \
+-e PASSWORD=test \
+-e PUID=1000 \
+-e PGID=1000 \
+-e DEFAULT_WORKSPACE=/config/workspace \
+-v $PWD:/config/workspace \
+geertvangeest/ngs-introduction-vscode:2023.11
+
+docker run \
+--rm \
+-p 3000:3000 \
 -e PUID=1000 \
 -e PGID=1000 \
 -e DEFAULT_WORKSPACE=/config/workdir \
 -v $PWD:/config/workdir \
-geertvangeest/ngs-introduction-vscode
+openvscode
+
+docker run \
+--rm \
+-p 3000:3000 \
+--name vscode_test \
+-e PASSWORD=test \
+-e PUID=1000 \
+-e PGID=1000 \
+-e DEFAULT_WORKSPACE=/config/workdir \
+--mount type=bind,source=$PWD,target=/config/workdir \
+openvscode
--- scripts/create_ngs_overview.R
+++ scripts/create_ngs_overview.R
@@ -3,27 +3,91 @@ library(dplyr)
 library(ggplot2)
 library(ggrepel)
 
-contr_list <- read_sheet("https://docs.google.com/spreadsheets/d/1fS6rmYjRAU26Y2en21hKoWyDn8jqiHMlsDs7bxZyA6Y/edit?usp=sharing"
+seq_method_table <- read_sheet("https://docs.google.com/spreadsheets/d/1fS6rmYjRAU26Y2en21hKoWyDn8jqiHMlsDs7bxZyA6Y/edit?usp=sharing"
 )
 
-p <- contr_list |> ggplot(
-  aes(x = log10(yield_high_gb),
-      y = log10(price_per_gb_min),
-      color = vendor,
-      label = type)) + 
-  xlab("log10(throughput (Gb))") +
-  ylab("log10(cost per Gb ($))") +
-  geom_point(size = 2.5) +
-  scale_color_brewer(palette = 'Paired') +
-  theme_classic() + 
-  geom_text_repel(max.overlaps = 5,
-                  color = 'darkgrey', box.padding = 0.3) +
-  theme(axis.text = element_text(size = 16),
-        axis.title = element_text(size = 16),
-        legend.text = element_text(size = 16),
-        legend.title = element_text(size = 16)) 
+vendors <- unique(seq_method_table$vendor)
+vendor_colors <- RColorBrewer::brewer.pal(length(vendors), "Paired")
+names(vendor_colors) <- vendors
+
+plot_seq_method <- function(df, x, y, shape = NULL, xlab = x, ylab = y,
+                            shape_lab = shape,
+                            vendor_colors) {
+  p <- ggplot(df,
+         aes(x = log10(.data[[x]]),
+             y = log10(.data[[y]]),
+             shape = if (is.null(shape)) NULL else .data[[shape]],
+             color = vendor,
+             label = type)) + 
+    xlab(xlab) +
+    ylab(ylab) +
+    geom_point(size = 2.5) +
+    scale_color_manual(values = vendor_colors) +
+    theme_classic() + 
+    geom_text_repel(max.overlaps = 5,
+                    color = 'darkgrey', box.padding = 0.3) +
+    theme(axis.text = element_text(size = 16),
+          axis.title = element_text(size = 16),
+          legend.text = element_text(size = 16),
+          legend.title = element_text(size = 16)) 
+  
+  if (!is.null(shape)) {
+    p <- p + labs(shape = shape_lab)
+  }
+  
+  return(p)
+}
+
+p <- seq_method_table |>
+  filter(vendor != "Sanger") |>
+  plot_seq_method(x = "yield_high_gb",
+                  y = "price_per_gb_min",
+                  xlab = "log10(throughput in Gb)",
+                  ylab = "log10(cost per Gb in $)",
+                  vendor_colors = vendor_colors)
 
 png("docs/assets/images/price_vs_yield.png",
     res = 300, width = 7.5, height = 6, units = "in")
 print(p)
 dev.off()
+
+p <- seq_method_table |>
+  plot_seq_method(x = "yield_high_gb",
+                  y = "price_per_gb_min",
+                  xlab = "log10(throughput in Gb)",
+                  ylab = "log10(cost per Gb in $)",
+                  vendor_colors = vendor_colors)
+
+png("docs/assets/images/price_vs_yield_incl_sanger.png",
+    res = 300, width = 7.5, height = 6, units = "in")
+print(p)
+dev.off()
+
+p <- seq_method_table |>
+  plot_seq_method(x = "read_length_max",
+                  y = "yield_high_gb",
+                  xlab = "log10(max read length)",
+                  ylab = "log10(throughput (Gb))",
+                  shape = "read_type",
+                  shape_lab = "read type",
+                  vendor_colors = vendor_colors)
+
+png("docs/assets/images/yield_vs_max_length.png",
+    res = 300, width = 7.5, height = 6, units = "in")
+print(p)
+dev.off()
+
+p <- seq_method_table |>
+  plot_seq_method(x = "read_length_max",
+                  y = "price_per_gb_min",
+                  xlab = "log10(max read length)",
+                  ylab = "log10(cost per Gb in $)",
+                  shape = "read_type",
+                  shape_lab = "read type",
+                  vendor_colors = vendor_colors)
+
+png("docs/assets/images/price_vs_max_length.png",
+    res = 300, width = 7.5, height = 6, units = "in")
+print(p)
+dev.off()
+  
